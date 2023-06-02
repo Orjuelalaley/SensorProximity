a13ab1a68da6abd090d537759d55e7a441d512f3
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-06-01 22:31:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sensorprox/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.sensorprox;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    Sensor proximitySensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference sensorsRef = database.getReference().child("Sensores");
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            float Proximity = event.values[0];
+            Map<String, Object> sensorValues = new HashMap<>();
+            if (Proximity <= 3) {
+                sensorValues.put("estado", 1);
+            } else {
+                sensorValues.put("estado", 0);
+            }
+            sensorsRef.updateChildren(sensorValues);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
